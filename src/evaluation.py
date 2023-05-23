Adjusts metric calculation. Still not sure if this is correct however.

src/evaluation.py
@@ -85,13 +85,14 @@ def get_error_dataset(images: torch.utils.data.DataLoader, x_hat: np.ndarray, im
 
 
 def _calculate_metrics(test_masks_orig_recon: np.ndarray, error_recon: np.ndarray):
-    fpr, tpr, thr = roc_curve(test_masks_orig_recon.flatten() > 0, error_recon.flatten())
+    test_masks = np.moveaxis(test_masks_orig_recon, -1, 1)
+    fpr, tpr, thr = roc_curve(test_masks.flatten() > 0, error_recon.flatten())
     true_auroc = auc(fpr, tpr)
-    precision, recall, thresholds = precision_recall_curve(test_masks_orig_recon.flatten() > 0,
+    precision, recall, thresholds = precision_recall_curve(test_masks.flatten() > 0,
                                                            error_recon.flatten())
     true_auprc = auc(recall, precision)
     f1 = 2 * (precision * recall) / (precision + recall)
-    true_f1 = f1.max()
+    true_f1 = np.max(f1)
     return {'auroc': true_auroc, 'auprc': true_auprc, 'f1': true_f1}
 
 
@@ -103,14 +104,15 @@ def nln(z, z_query, neighbours):
     return neighbours_dist, indices, neighbour_mask
 
 
-def nln_errors(test_dataset: torch.utils.data.DataLoader, x_hat, neighbours_idx,
+def nln_errors(test_dataset: torch.utils.data.DataLoader, x_hat, x_hat_train, neighbours_idx,
                neighbour_mask):
-    test_images = test_dataset.dataset[:][1].cpu().detach().numpy()
+    test_images = test_dataset.dataset[:][0].cpu().detach().numpy()
     test_images_stacked = np.stack([test_images] * neighbours_idx.shape[-1], axis=1)
-    neighbours = x_hat[neighbours_idx]
+    neighbours = x_hat_train[neighbours_idx]
     error_nln = test_images_stacked - neighbours
     error_recon = test_images - x_hat
-    error = np.mean(error_nln, axis=1)  # nanmean for frNN
+    error = np.abs(error_nln)
+    error = np.mean(error, axis=1)  # nanmean for frNN
     error[neighbour_mask] = error_recon[neighbour_mask]
     return error
 
@@ -129,6 +131,7 @@ def get_dists(neighbours_dist, original_size: int, patch_size: int = None):
 
 def calculate_metrics(model: Autoencoder,
                       test_masks_original: np.ndarray,
+                      train_dataset: torch.utils.data.DataLoader,
                       test_dataset: torch.utils.data.DataLoader, neighbours: int, batch_size: int,
                       model_name: str, model_type: str, anomaly_type: str,
                       latent_dimension: int, original_size: int, patch_size: int = None,
@@ -138,9 +141,11 @@ def calculate_metrics(model: Autoencoder,
                                             original_size, patch_size)
     test_masks_reconstructed = reconstruct_patches(
         test_dataset.dataset[:][1].cpu().detach().numpy(), original_size, patch_size)
-    z = infer(model.encoder, test_dataset, batch_size, latent_dimension, True)
+
+    z = infer(model.encoder, train_dataset, batch_size, latent_dimension, True)
     z_query = infer(model.encoder, test_dataset, batch_size, latent_dimension, True)
 
+    x_hat_train = infer(model, train_dataset, batch_size, latent_dimension, False)
     x_hat = infer(model, test_dataset, batch_size, latent_dimension, False)
     x_hat_recon = reconstruct_patches(x_hat, original_size, patch_size)
 
@@ -150,7 +155,7 @@ def calculate_metrics(model: Autoencoder,
 
     ae_metrics = _calculate_metrics(test_masks_original, error_recon)
     neighbours_dist, neighbours_idx, neighbour_mask = nln(z, z_query, neighbours)
-    nln_error = nln_errors(test_dataset, x_hat, neighbours_idx, neighbour_mask)
+    nln_error = nln_errors(test_dataset, x_hat, x_hat_train, neighbours_idx, neighbour_mask)
 
     if patch_size:
         if nln_error.ndim == 4:
@@ -186,11 +191,11 @@ def calculate_metrics(model: Autoencoder,
     return ae_metrics, nln_metrics, dist_metrics
 
 
-def evaluate_model(model, test_masks, test_dataset, neighbours, batch_size,
+def evaluate_model(model, test_masks, test_dataset, train_dataset, neighbours, batch_size,
                    latent_dimension, original_size, patch_size, model_name, model_type,
                    anomaly_type, dataset):
     ae_metrics, nln_metrics, dist_metrics = calculate_metrics(model, test_masks,
-                                                              test_dataset, neighbours,
+                                                              train_dataset, test_dataset, neighbours,
                                                               batch_size, model_name, model_type,
                                                               anomaly_type, latent_dimension,
                                                               original_size, patch_size, dataset)