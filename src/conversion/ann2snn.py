8ed8b57c789e14caf06026e8392a4bbd74bf1a20
pritchardn <21726929@student.uwa.edu.au>
2023-06-14 15:02:03 +0800

    Moves example ann2snn test to separate file.

--- src/conversion/ann2snn.py
+++ src/conversion/ann2snn.py
@@ -1,13 +1,11 @@
+import math
+
+import matplotlib.animation as animation
 import numpy as np
 import torch
-import math
 from matplotlib import pyplot as plt
-import matplotlib.animation as animation
 from spikingjelly.activation_based import ann2snn
 from tqdm import tqdm
-
-from data import load_data, process_into_dataset
-from models import Autoencoder
 
 
 def animated_plotting(out_images):
@@ -97,28 +95,3 @@ def convert_to_snn(model, test_data_loader):
 def test_snn_model(snn_model, test_data_loader):
     full_output = infer_snn(snn_model, test_data_loader, runtime=64)
     plot_outputs(full_output)
-
-
-if __name__ == "__main__":
-    train_x, train_y, test_x, test_y, rfi_models = load_data()
-    config_vals = {
-        'batch_size': 32,
-        'threshold': 10,
-        'patch_size': 32,
-        'patch_stride': 32,
-        'num_layers': 2,
-        'latent_dimension': 32,
-        'num_filters': 32
-    }
-    model_path = 'autoencoder.pt'
-    test_dataset, test_labels_orig = process_into_dataset(test_x, test_y, batch_size=config_vals['batch_size'],
-                                        mode='HERA', threshold=config_vals['threshold'],
-                                        patch_size=config_vals['patch_size'],
-                                        stride=config_vals['patch_stride'])
-
-    model = Autoencoder(config_vals['num_layers'], config_vals['latent_dimension'],
-                        config_vals['num_filters'], test_dataset.dataset[0][0].shape)
-    model.load_state_dict(torch.load(model_path))
-    model.eval()
-    snn_model = convert_to_snn(model, test_dataset)
-    test_snn_model(snn_model, test_dataset)
