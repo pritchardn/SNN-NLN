e87decb0ecb0ade0827d1c175597313e42772b58
Nicholas Pritchard <30886786+pritchardn@users.noreply.github.com>
2023-07-19 09:18:36 +0800

    Adds function to collate result files to csv.

---
+++ src/result_collation.py
@@ -0,0 +1,43 @@
+import glob
+import json
+
+import pandas as pd
+import os
+import csv
+
+from utils import model_name_to_config_vals
+
+MODELS = ["SDAE", "DAE"]
+
+
+def write_csv_output_from_dict(outputdir: str, filename: str, data: list, headers: list):
+    os.makedirs(outputdir, exist_ok=True)
+    with open(f"{outputdir}{os.sep}{filename}.csv", 'w') as ofile:
+        csv_writer = csv.DictWriter(ofile, fieldnames=headers)
+        csv_writer.writeheader()
+        for row in data:
+            csv_writer.writerow(row)
+
+
+def collate_results(outputdir: str) -> list:
+    results = []
+    for model in MODELS:
+        model_outputdir = os.path.join(outputdir, model, "MISO")
+        for filename in os.listdir(model_outputdir):
+            trial_vals = model_name_to_config_vals(filename)
+            for result_filename in glob.glob(
+                    os.path.join(model_outputdir, filename, 'metrics.json')):
+                with open(result_filename, 'r') as f:
+                    result_data = json.load(f)
+                    if "nln" in result_data:
+                        trial_vals.update(result_data["nln"])
+                    else:
+                        trial_vals.update(result_data)
+            print(trial_vals)
+            results.append(trial_vals)
+    return results
+
+
+if __name__ == "__main__":
+    results = collate_results("outputs")
+    write_csv_output_from_dict("outputs", "results", results, results[0].keys())
--- src/utils.py
+++ src/utils.py
@@ -15,6 +15,31 @@ def generate_model_name(config_vals: dict):
     return model_name
 
 
+def model_name_to_config_vals(dirname: str) -> dict:
+    traits = dirname.split("_")
+    config_vals = {"model_type": traits[0],
+                   "anomaly_type": traits[1], "dataset": traits[2],
+                   "latent_dimension": int(traits[3]),
+                   "num_layers": int(traits[4]), "threshold": int(traits[5])}
+    if config_vals["model_type"] == "DAE":
+        if len(traits) > 6:
+            config_vals["excluded_rfi"] = traits[6].join("_").join(traits[6:])
+        else:
+            config_vals["excluded_rfi"] = ""
+        config_vals["time_length"] = ""
+        config_vals["average_n"] = ""
+    elif config_vals["model_type"] == "SDAE":
+        if len(traits) > 8:
+            config_vals["excluded_rfi"] = traits[6].join("_").join(traits[6:8])
+            config_vals["time_length"] = int(traits[8])
+            config_vals["average_n"] = int(traits[9])
+        else:
+            config_vals["time_length"] = int(traits[6])
+            config_vals["average_n"] = int(traits[7])
+            config_vals["excluded_rfi"] = ""
+    return config_vals
+
+
 def generate_output_dir(config_vals: dict):
     output_dir = os.path.join("outputs", config_vals["model_type"], config_vals["anomaly_type"],
                               config_vals["model_name"])
