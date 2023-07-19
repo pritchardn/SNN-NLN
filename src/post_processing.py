f92987b18488bd8b493f8ebbad26870a9c922180
Nicholas Pritchard <30886786+pritchardn@users.noreply.github.com>
2023-07-19 10:11:11 +0800

    Renames result_collation and adds threshold plot.

--- src/post_processing.py
+++ src/post_processing.py
@@ -4,6 +4,7 @@ import json
 import pandas as pd
 import os
 import csv
+import matplotlib.pyplot as plt
 
 from utils import model_name_to_config_vals
 
@@ -38,10 +39,42 @@ def collate_results(outputdir: str) -> list:
     return results
 
 
+def make_threshold_plot(results: pd.DataFrame):
+    print(results.head())
+    print(len(results))
+    sub_results = results[pd.isna(results["excluded_rfi"])]
+    print(results["excluded_rfi"].unique())
+    print(len(sub_results))
+    models = results["model_type"].unique()
+    width = 0.25  # 1/len(models)
+    fig, axs = plt.subplots(1, 3, figsize=(15, 5))
+    i = -1
+    # TODO: Multiple trials and therefore std-dev error bars
+    # TODO: Filter results for best time_length and average_n
+    for model in models:
+        model_results = sub_results[sub_results["model_type"] == model].groupby("threshold").mean(
+            "auroc", "auprc", "f1")
+        axs[0].bar(model_results.index - width / 2 * i, model_results["auroc"], width=width,
+                   label=model)
+        axs[1].bar(model_results.index - width / 2 * i, model_results["auprc"], width=width,
+                   label=model)
+        axs[2].bar(model_results.index - width / 2 * i, model_results["f1"], width=width,
+                   label=model)
+        i = -i
+        print(model_results)
+    axs[0].legend()
+    axs[0].set_ylabel("AUROC")
+    axs[1].set_ylabel("AUPRC")
+    axs[2].set_ylabel("F1")
+    for ax in axs:
+        ax.set_xlabel("AOFlagger Threshold")
+    plt.savefig("outputs/threshold_plot.png", dpi=300)
+
+
 def collate_reuslts():
     results = collate_results("outputs")
     write_csv_output_from_dict("outputs", "results", results, results[0].keys())
 
 
 if __name__ == "__main__":
-    collate_reuslts()
+    make_threshold_plot(pd.read_csv("outputs/results.csv"))
