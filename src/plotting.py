47b43860844dea3a9ecc6ee1ffa934cc9d275039
pritchardn <21726929@student.uwa.edu.au>
2023-07-21 15:47:17 +0800

    Fixes and simplifies interim plotting.

--- src/plotting.py
+++ src/plotting.py
@@ -1,11 +1,13 @@
 import math
 import os
-import numpy as np
 
+import numpy as np
 import torch.nn as nn
 import wandb
 from matplotlib import pyplot as plt
+from mpl_toolkits.axes_grid1 import ImageGrid
 from torch.utils.data import TensorDataset
+
 from config import WANDB_ACTIVE, DEVICE
 
 
@@ -19,39 +21,32 @@ def remove_stripes(image: np.ndarray):
 
 
 def plot_intermediate_images(
-    auto_encoder: nn.Module,
-    dataset: TensorDataset,
-    epoch: int,
-    title: str,
-    outputdir: str,
-    batch_size: int,
+        auto_encoder: nn.Module,
+        dataset: TensorDataset,
+        epoch: int,
+        title: str,
+        outputdir: str,
+        batch_size: int,
 ):
     for batch, (x, y) in enumerate(dataset):
         x = x.to(DEVICE)
         predictions = auto_encoder(x).cpu().detach().numpy()
-        plt.figure(figsize=(10, 10))
-        for i in range(min(batch_size, 24)):
-            sub_range = int(math.ceil(math.sqrt(batch_size)))
-            plt.subplot(sub_range, sub_range, i + 1)
-            if predictions.shape[1] == 1:  # 1 channel only
-                plt.imshow(remove_stripes(predictions[i, 0, :, :]) * 127.5 + 127.5)
-
-            if predictions.shape[1] == 3:  # RGB
-                plt.imshow(predictions[i, ...], vmin=0, vmax=1)
-            plt.axis("off")
         x = x.cpu().detach().numpy()
-        for i in range(min(batch_size, 24)):  # , 48
-            # TODO: Get the range back up to 24 so both panes are visible
-            sub_range = int(math.ceil(math.sqrt(batch_size)))
-            plt.subplot(sub_range, sub_range, i + 1)
-            if x.shape[1] == 1:
-                plt.imshow(x[i - min(batch_size, 24), 0, :, :] * 127.5 + 127.5)
-            plt.axis("off")
-
+        images = np.concatenate((x, predictions), axis=0)
+        side_length = int(math.sqrt(batch_size))
+        fig = plt.figure(figsize=(side_length, side_length * 2))
+        grid = ImageGrid(fig, 111,  # similar to subplot(111)
+                         nrows_ncols=(side_length * 2, side_length),
+                         axes_pad=0.1,  # pad between axes in inch.
+                         )
+        for i in range(2 * batch_size):
+            # Iterating over the grid returns the Axes.
+            grid[i].imshow(remove_stripes(images[i, 0, :, :]) * 127.5 + 127.5, aspect='auto')
+            grid[i].axis('off')
+        plt.axis('off')
         output_path = os.path.join(outputdir, "results")
         os.makedirs(output_path, exist_ok=True)
         plot_filename = f"{output_path}{os.sep}{title}-{epoch}.png"
-        plt.tight_layout()
         plt.savefig(plot_filename)
         if WANDB_ACTIVE:
             wandb.log({"example-reconstruction": wandb.Image(plot_filename)})
