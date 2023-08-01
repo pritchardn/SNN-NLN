8ec93b95d2a74b12467be0aa98f7f54f19298b5f
pritchardn <21726929@student.uwa.edu.au>
2023-08-01 11:39:42 +0800

    Improves code-style in loss.py

--- src/loss.py
+++ src/loss.py
@@ -1,21 +1,33 @@
+"""
+Loss functions for the model.
+Copyright (c) 2023, Nicholas Pritchard <nicholas.pritchard@icrar.org>
+"""
+
 import torch
-import torch.nn as nn
+from torch import nn
 
 mse = nn.MSELoss()
-mae = nn.L1Loss()
-bce = nn.BCELoss()
 
 
-def ae_loss(x, y):
-    return mse(x, y)
+def ae_loss(x_data, y_data):
+    """
+    Calculates the loss for the autoencoder.
+    """
+    return mse(x_data, y_data)
 
 
-def discriminator_loss(x, y, weight):
-    real_loss = mse(torch.ones_like(x), x)
-    fake_loss = mse(torch.ones_like(y), y)
+def discriminator_loss(x_data, y_data, weight):
+    """
+    Calculates the loss for the discriminator.
+    """
+    real_loss = mse(torch.ones_like(x_data), x_data)
+    fake_loss = mse(torch.ones_like(y_data), y_data)
     total_loss = real_loss + fake_loss
     return total_loss * weight
 
 
-def generator_loss(y, weight):
-    return torch.mean(mse(torch.ones_like(y), y)) * weight
+def generator_loss(y_data, weight):
+    """
+    Calculates the loss for the generator.
+    """
+    return torch.mean(mse(torch.ones_like(y_data), y_data)) * weight
