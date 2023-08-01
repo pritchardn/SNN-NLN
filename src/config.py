c7036e7a7b9e890dd1a9965e7e33bdc3eadf1895
pritchardn <21726929@student.uwa.edu.au>
2023-08-01 11:41:06 +0800

    Improves code-style in config.py

--- src/config.py
+++ src/config.py
@@ -1,3 +1,7 @@
+"""
+Contains global variables used throughout the proejct.
+Copyright (c) 2023, Nicholas Pritchard <nicholas.pritchard@icrar.org>
+"""
 import torch
 
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
