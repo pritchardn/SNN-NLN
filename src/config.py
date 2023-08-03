d7dbc16f2bb6dbde1567686c0570b8a84c449f24
pritchardn <21726929@student.uwa.edu.au>
2023-08-03 10:37:21 +0800

    Adjusts standard parameters based on output of latest optuna run.

--- src/config.py
+++ src/config.py
@@ -9,16 +9,16 @@ import torch
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
 STANDARD_PARAMS = {
-    "batch_size": 16,
-    "epochs": 5,
-    "ae_learning_rate": 1.89e-4,
-    "gen_learning_rate": 7.90e-4,
-    "disc_learning_rate": 9.49e-4,
+    "batch_size": 64,
+    "epochs": 15,
+    "ae_learning_rate": 0.0004215152200409049,
+    "gen_learning_rate": 0.0008860574544781892,
+    "disc_learning_rate": 0.0009173638064067504,
     "optimizer": "RMSprop",
     "num_layers": 5,
     "latent_dimension": 32,
     "num_filters": 16,
-    "neighbours": 20,
+    "neighbours": 17,
     "patch_size": 32,
     "patch_stride": 32,
     "threshold": 10,
