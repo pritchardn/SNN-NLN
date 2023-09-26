d46a3ac954d4d4d777d043ea8a1b8d5d41f8a629
pritchardn <21726929@student.uwa.edu.au>
2023-09-26 14:13:35 +0800

    Adds setonix snn runfiles.

---
+++ src/hpc/setonix_snn_threshold.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH --job-name=SNN-NLN
+#SBATCH --nodes=1
+#SBATCH --mem=230GB
+#SBATCH --time=10:00:00
+#SBATCH --output=snn_%A_%a.out
+#SBATCH --error=snn_%A_%a.err
+#SBATCH --array=0-9
+#SBATCH --partition=work
+#SBATCH --account=pawsey0411
+
+
+module load python/3.10.10
+
+cd /software/projects/pawsey0411/npritchard/setonix/2023.08/python/SNN-NLN/src
+source /software/projects/pawsey0411/npritchard/setonix/2023.08/python/snn-nln/bin/activate
+
+export NUM_TRIALS=1
+export TASK_TYPE="SNN"
+export MODEL_TYPE="DAE-THRESHOLD"
+export OUTPUT_DIR="/scratch/pawsey0411/npritchard/outputs/noise"
+export DATA_DIR="/scratch/pawsey0411/npritchard/data"
+export INPUT_DIR="/scratch/pawsey0411/npritchard/data"
+export DATASET="HERA"
+export TIME_LENGTH=256
+export AVERAGE_N=128
+
+srun -N 1 -n 1 -c 32 python3 main_hpc.py
