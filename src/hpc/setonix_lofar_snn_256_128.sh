2db1e22582f3a64f6abceb65484c3e581e6990a7
pritchardn <21726929@student.uwa.edu.au>
2023-09-29 14:29:38 +0800

    Updates setonix runfiles and adds lofar snn runfiles.

--- src/hpc/setonix_lofar_snn_256_128.sh
+++ src/hpc/setonix_lofar_snn_256_128.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH --job-name=SNN-NLN
 #SBATCH --nodes=1
-#SBATCH --time=0:25:00
+#SBATCH --mem=230GB
+#SBATCH --time=1:00:00
 #SBATCH --output=snn_%A_%a.out
 #SBATCH --error=snn_%A_%a.err
-#SBATCH --array=0-0
+#SBATCH --array=0-9
 #SBATCH --partition=work
-#SBATCH --mem=64GB
 #SBATCH --account=pawsey0411
 
 
@@ -16,10 +16,13 @@ cd /software/projects/pawsey0411/npritchard/setonix/2023.08/python/SNN-NLN/src
 source /software/projects/pawsey0411/npritchard/setonix/2023.08/python/snn-nln/bin/activate
 
 export NUM_TRIALS=1
-export TASK_TYPE="STANDARD"
-export OUTPUT_DIR="/scratch/pawsey0411/npritchard/outputs/hera"
+export TASK_TYPE="SNN"
+export MODEL_TYPE="DAE"
+export OUTPUT_DIR="/scratch/pawsey0411/npritchard/outputs/lofar"
 export DATA_DIR="/scratch/pawsey0411/npritchard/data"
 export INPUT_DIR="/scratch/pawsey0411/npritchard/data"
 export DATASET="LOFAR"
+export TIME_LENGTH=256
+export AVERAGE_N=128
 
 srun -N 1 -n 1 -c 32 python3 main_hpc.py
--- src/hpc/setonix_optuna_lofar.sh
+++ src/hpc/setonix_optuna_lofar.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 #SBATCH --job-name=SNN-NLN
 #SBATCH --nodes=1
-#SBATCH --time=0:25:00
+#SBATCH --time=96:00:00
 #SBATCH --output=snn_%A_%a.out
 #SBATCH --error=snn_%A_%a.err
 #SBATCH --array=0-0
-#SBATCH --partition=work
-#SBATCH --mem=64GB
+#SBATCH --partition=long
+#SBATCH --mem=128GB
 #SBATCH --account=pawsey0411
-
 
 module load python/3.10.10
 
 cd /software/projects/pawsey0411/npritchard/setonix/2023.08/python/SNN-NLN/src
 source /software/projects/pawsey0411/npritchard/setonix/2023.08/python/snn-nln/bin/activate
 
-export NUM_TRIALS=1
-export TASK_TYPE="STANDARD"
-export OUTPUT_DIR="/scratch/pawsey0411/npritchard/outputs/hera"
+export NUM_TRIALS=16
+export TASK_TYPE="OPTUNA"
+export OUTPUT_DIR="/scratch/pawsey0411/npritchard/outputs/optuna"
 export DATA_DIR="/scratch/pawsey0411/npritchard/data"
 export INPUT_DIR="/scratch/pawsey0411/npritchard/data"
 export DATASET="LOFAR"
