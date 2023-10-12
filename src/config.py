a3ad0702c89b975564fd873ff321d3434d07e155
pritchardn <21726929@student.uwa.edu.au>
2023-10-12 11:29:01 +0800

    Updates tabascal params from optuna hyper-parameter run.

--- src/config.py
+++ src/config.py
@@ -60,16 +60,16 @@ LOFAR_PARAMS = {
 }
 
 TABASCAL_PARAMS = {
-    "batch_size": 32,
-    "epochs": 17,
-    "ae_learning_rate": 0.001589310185217798,
-    "gen_learning_rate": 0.0017737115327998176,
-    "disc_learning_rate": 0.00016792476164863897,
+    "batch_size": 16,
+    "epochs": 13,
+    "ae_learning_rate": 0.00042580710933189434,
+    "gen_learning_rate": 0.009392349641041532,
+    "disc_learning_rate": 0.005050946755870783,
     "optimizer": "Adam",
     "num_layers": 2,
-    "latent_dimension": 64,
+    "latent_dimension": 16,
     "num_filters": 16,
-    "neighbours": 21,
+    "neighbours": 22,
     "patch_size": 32,
     "patch_stride": 32,
     "threshold": 10,
