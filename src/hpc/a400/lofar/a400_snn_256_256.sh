57181c4be4a0b3188df3752452e6d95e66d8d9cb
Nicholas Pritchard <30886786+pritchardn@users.noreply.github.com>
2023-10-27 17:23:13 +0800

    Adds LOFAR SNN runfiles for a400

---
+++ src/hpc/a400/lofar/a400_snn_256_256.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --job-name=SNN-NLN
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=128GB
+#SBATCH --time=24:00:00
+#SBATCH --output=snn_%A_%a.out
+#SBATCH --error=snn_%A_%a.err
+#SBATCH --array=0-9%2
+#SBATCH --gres=gpu:1
+
+
+cd /home/npritchard/SNN-NLN/src
+source /home/npritchard/snn-nln-a100/bin/activate
+
+export NUM_TRIALS=10
+export TASK_TYPE="SNN"
+export OUTPUT_DIR="/home/npritchard/outputs/lofar"
+export DATA_DIR="/home/npritchard/data"
+export INPUT_DIR="/home/npritchard/data"
+export DATASET="LOFAR"
+export TIME_LENGTH=256
+export AVERAGE_N=256
+
+python3 main_hpc.py
